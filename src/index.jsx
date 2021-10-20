2bfc2e8579d74ca98038494e99f200c4f4e62118
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-10-20 13:15:11 +0300

    Added material ui theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+
 import App from './App/App';
+import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <StyledThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </StyledThemeProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/theme.js
@@ -0,0 +1,29 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#345a80',
+    },
+    secondary: {
+      main: '#e3322b',
+    },
+    type: 'light',
+    background: {
+      default: '#f6f6f6',
+      paper: '#fff',
+    },
+    text: {
+      primary: '#345a80',
+      secondary: '#000',
+    },
+  },
+  typography: {
+    fontFamily: '"Montserrat", "Open Sans"',
+    h1: {
+      fontSize: 24,
+      fontWeight: 700,
+      lineHeight: 1.54,
+    },
+  },
+});
