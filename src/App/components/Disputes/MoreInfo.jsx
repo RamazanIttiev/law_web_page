d5b3c47193eb93ced609b78fe9bfe15fca8264fc
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-10-25 22:55:47 +0300

    more info dialog set up

---
+++ src/App/components/Disputes/MoreInfo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Backdrop, Dialog, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import styled from 'styled-components';
+import FormButton from '../../Ui/FormButton';
+
+const Title = styled(DialogTitle).attrs({ variant: 'h1', component: 'h2' })``;
+
+const Text = styled(DialogContentText).attrs({ variant: 'body', component: 'div' })``;
+
+const MoreInfo = ({ open, handleClose, title, paragraph }) => (
+  <Dialog
+    open={open}
+    onClose={handleClose}
+    closeAfterTransition
+    BackdropComponent={Backdrop}
+    BackdropProps={{
+      timeout: 500,
+    }}
+  >
+    <Title sx={{ fontSize: '28px' }}>{title}</Title>
+    <DialogContent>
+      <Text sx={{ color: '#000', marginBottom: '24px' }}>{paragraph}</Text>
+      <FormButton text="Получить консультацию" />
+    </DialogContent>
+  </Dialog>
+);
+
+export default MoreInfo;
