3ce7d595ce29249448ba2fb7d2b7ef7b78b54f0d
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-10-27 21:55:02 +0300

    ArrowBack set up

--- src/App/components/Insurance/Case/Promo.jsx
+++ src/App/components/Insurance/Case/Promo.jsx
@@ -1,12 +1,14 @@
+import { ArrowBack } from '@mui/icons-material';
 import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import PromoBgr from '../../../../assets/Promo_bgr.jpeg';
 import Button from '../../../Ui/FormButton';
 
 const Wrapper = styled.div`
   background-image: -webkit-linear-gradient(top, rgb(52, 90, 128), rgba(163, 163, 163, 0.7));
-  padding: 32px 0 56px;
+  padding: 24px 0 56px;
 `;
 
 const Title = styled(Typography).attrs({ variant: 'h1' })``;
@@ -20,28 +22,41 @@ const ListItem = styled.li`
   margin: 0 24px;
 `;
 
-const Promo = ({ title, about }) => (
-  <Box sx={{ background: `url(${PromoBgr})`, color: '#fff' }}>
-    <Wrapper>
-      <Container>
-        <Title
-          sx={{
-            marginBottom: '56px',
-            color: '#fff',
-            textAlign: 'start',
-          }}
-        >
-          {title}
-        </Title>
-        <List>
-          {about.map(listItem => (
-            <ListItem key={listItem}>{listItem}</ListItem>
-          ))}
-        </List>
-        <Button margin="72" text="Получите бесплатную консультацию" />
-      </Container>
-    </Wrapper>
-  </Box>
-);
+const Promo = ({ title, about }) => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    if (history.length === 1) {
+      history.push('/');
+    } else {
+      history.goBack();
+    }
+  };
+
+  return (
+    <Box sx={{ background: `url(${PromoBgr})`, color: '#fff' }}>
+      <Wrapper>
+        <Container>
+          <ArrowBack onClick={handleClick} sx={{ mb: 3, cursor: 'pointer' }} />
+          <Title
+            sx={{
+              marginBottom: '56px',
+              color: '#fff',
+              textAlign: 'start',
+            }}
+          >
+            {title}
+          </Title>
+          <List>
+            {about.map(listItem => (
+              <ListItem key={listItem}>{listItem}</ListItem>
+            ))}
+          </List>
+          <Button margin="72" text="Получите бесплатную консультацию" />
+        </Container>
+      </Wrapper>
+    </Box>
+  );
+};
 
 export default Promo;
