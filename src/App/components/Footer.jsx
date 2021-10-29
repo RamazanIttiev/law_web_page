5f5066083e5260a9b34b72d7af8a6cdf200aa583
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-10-29 12:10:20 +0300

    footer tag added

--- src/App/components/Footer.jsx
+++ src/App/components/Footer.jsx
@@ -36,32 +36,34 @@ const Link = styled.a`
 `;
 
 const Contacts = () => (
-  <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }} alignItems="center">
-    <Grid item xs={12} sm={6} md={6}>
-      <Info>
-        <Typography>
-          <Bold>Мы работаем:</Bold> с 9 до 21, пн-сб. Приём заявок 24/7
-        </Typography>
-        <Typography>
-          <Bold>Телефон:</Bold>
-          <Link href="tel:+7 932 777 77 52">+7 932 777 77 52</Link>
-        </Typography>
-        <Typography>
-          <Bold>Email:</Bold>
-          <Link href="mailto:rightsprotectionrf@yandex.ru">rightsprotectionrf@yandex.ru</Link>
-        </Typography>
-      </Info>
+  <footer>
+    <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }} alignItems="center">
+      <Grid item xs={12} sm={6} md={6}>
+        <Info>
+          <Typography>
+            <Bold>Мы работаем:</Bold> с 9 до 21, пн-сб. Приём заявок 24/7
+          </Typography>
+          <Typography>
+            <Bold>Телефон:</Bold>
+            <Link href="tel:+7 932 777 77 52">+7 932 777 77 52</Link>
+          </Typography>
+          <Typography>
+            <Bold>Email:</Bold>
+            <Link href="mailto:rightsprotectionrf@yandex.ru">rightsprotectionrf@yandex.ru</Link>
+          </Typography>
+        </Info>
+      </Grid>
+      <Grid item xs={12} sm={6} md={6}>
+        <Iframe
+          title="map"
+          src="https://yandex.ru/map-widget/v1/?um=constructor%3A2c0fb630e9a8661cdf0bf01a87572d46ded932a066be8b02d7247754a098a2b6&amp;source=constructor"
+          width="781"
+          height="400"
+          frameBorder="0"
+        />
+      </Grid>
     </Grid>
-    <Grid item xs={12} sm={6} md={6}>
-      <Iframe
-        title="map"
-        src="https://yandex.ru/map-widget/v1/?um=constructor%3A2c0fb630e9a8661cdf0bf01a87572d46ded932a066be8b02d7247754a098a2b6&amp;source=constructor"
-        width="781"
-        height="400"
-        frameBorder="0"
-      />
-    </Grid>
-  </Grid>
+  </footer>
 );
 
 export default Contacts;
