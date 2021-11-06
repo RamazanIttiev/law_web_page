b6e545998d4267752007e336e4bbc25f7ee0b1be
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-11-06 11:24:47 +0300

    added scrollbar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Disputes from './pages/Disputes';
@@ -8,30 +8,69 @@ import Fraud from './pages/Fraud';
 import Insurance from './pages/Insurance';
 import Cases from './components/Insurance/Cases';
 import Error from './pages/404';
+import ScrollBar from './components/ScrollBar';
 
-const App = () => (
-  <>
-    <Header />
-    <Switch>
-      <Route exact path="/">
-        <Main />
-      </Route>
-      <Route path="/fraud">
-        <Fraud />
-      </Route>
-      <Route path="/insurance">
-        <Insurance />
-      </Route>
-      <Route path="/disputes">
-        <Disputes />
-      </Route>
-      <Route path="/insuranceCase/:id" component={Cases} />
-      <Route path="*">
-        <Error />
-      </Route>
-    </Switch>
-    <Footer />
-  </>
-);
+const App = () => {
+  const [scrollPostion, setScrollPostion] = useState(10);
+
+  const getDocHeight = () =>
+    Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.body.clientHeight,
+      document.documentElement.clientHeight,
+    );
+
+  const calculateScrollDistance = () => {
+    const scrollTop = window.pageYOffset;
+    const winHeight = window.innerHeight;
+    const docHeight = getDocHeight();
+
+    const totalDocScrollLength = docHeight - winHeight;
+    const scrollBarPostion = Math.floor((scrollTop / totalDocScrollLength) * 100);
+
+    setScrollPostion(scrollBarPostion);
+  };
+
+  const listenToScrollEvent = () => {
+    document.addEventListener('scroll', () => {
+      requestAnimationFrame(() => {
+        calculateScrollDistance();
+      });
+    });
+  };
+
+  useEffect(() => {
+    listenToScrollEvent();
+  }, [scrollPostion]);
+
+  return (
+    <>
+      <ScrollBar scroll={scrollPostion} />
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/fraud">
+          <Fraud />
+        </Route>
+        <Route path="/insurance">
+          <Insurance />
+        </Route>
+        <Route path="/disputes">
+          <Disputes />
+        </Route>
+        <Route path="/insuranceCase/:id" component={Cases} />
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
