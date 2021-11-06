7f735bdcdba6ff9eba517ed6830b177c6f152ac6
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-11-06 18:41:35 +0300

    removed examples from disputes

--- src/App/pages/Disputes.jsx
+++ src/App/pages/Disputes.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import Description from '../components/Disputes/Description';
 import Promo from '../components/Disputes/Promo';
-import Examples from '../components/Carousel';
-import { disputeCases, disputeExamples } from '../data/disputeCases';
+import { disputeCases } from '../data/disputeCases';
 import Cases from '../components/Cases';
 import Form from '../components/Form';
 
@@ -12,7 +11,6 @@ const Disputes = () => (
     <Promo />
     <Description />
     <Cases data={disputeCases} />
-    <Examples examples={disputeExamples} />
     <Form title="Оставяьте заявку и получите консультацию нашего юриста" />
     <Helmet>
       <meta name="description" content="Жилищные вопросы" />
